d84fb4d8071fd2c2c62e9e71ec865a0fd735f0cc
Valvas <valvas.vincent@outlook.fr>
2017-10-15 11:36:24 +0200

    Working on database creation

--- init/database/generation.js
+++ init/database/generation.js
@@ -1,8 +1,6 @@
 'use strict';
 
-let config = require('../../json/database/config');
-
-let databases = require(`../../json/database/${config['techno']}`);
+let databases = require(`../../json/database/mysql`);
 
 /****************************************************************************************************/
 
@@ -11,39 +9,73 @@ module.exports.createDatabases = function(connector, callback)
   let x = 0;
   let array = [];
 
-  let loop = function(database)
+  let loop = function(databaseName, databaseContent)
   {
-    if(config['techno'] == 'mysql')
+    connector.query(`CREATE DATABASE IF NOT EXISTS ${databaseName}`, function(err, result)
     {
-      connector.query(`CREATE DATABASE IF NOT EXISTS ${Object.keys(databases)[x]}`, function(err, result)
-      {
-        err ? callback(false, err.message) : array.push(`INFO : database "${Object.keys(databases)[x]}" created !`);
-  
-        createAllTables(databases[x], function(result, code, message)
-        {
-          result == false ? callback(false, code, message) : x++;
+      err ? callback(false, err.message) : array.push(`INFO : database "${databaseName}" created !`);
 
-          Object.keys(databases)[x] == undefined ? callback(true, array.join('\n')) : loop(Object.keys(databases)[x]);
+      connector.query(`USE ${databaseName}`, function(err, result)
+      {
+        err ? callback(false, err.message) :
+        
+        createAllTables(databaseContent, databaseName, connector, function(result, message)
+        {
+          result == false ? callback(false, message) : x++;
+      
+          array.push(message);
+      
+          Object.keys(databases)[x] == undefined ? callback(true, array.join('\n')) : loop(Object.keys(databases)[x], databases[Object.keys(databases)[x]]);
         });
       });
-    }
+    });
   };
 
-  databases[x] == undefined ? callback(true, 'INFO : no database to create !') : loop(Object.keys(databases)[x]);
+  Object.keys(databases)[x] == undefined ? callback(true, 'INFO : no database to create !') : loop(Object.keys(databases)[x], databases[Object.keys(databases)[x]]);
 }
 
 /****************************************************************************************************/
 
-function createAllTables(database, callback)
+function createAllTables(database, databaseName, connector, callback)
 {
   let x = 0;
+  let array = [];
 
-  let loop = function(table)
+  let loop = function(tableName, tableContent)
   {
+    createTable(tableContent, function(result, message)
+    {
+      result == false ? callback(false, message) : array.push(`[${databaseName}] : table "${tableName}" created !`);
 
+      connector.query(`CREATE TABLE IF NOT EXISTS ${tableName} (${result})`, function(err, data)
+      {
+        err != undefined ? callback(false, err.message) : x++;
+        
+        Object.keys(database)[x] == undefined ? callback(true, array.join('\n')) : loop(Object.keys(database)[x], database[Object.keys(database)[x]]);
+      });
+    });
   };
 
-  Object.keys(database)[x] == undefined ? callback(true, `INFO : no tables to create for database "${database}" !`) : loop(database[Object.keys(database)[x]]);
+  Object.keys(database)[x] == undefined ? callback(true, `INFO : no tables to create for this database !`) : loop(Object.keys(database)[x], database[Object.keys(database)[x]]);
+}
+
+/****************************************************************************************************/
+
+function createTable(table, callback)
+{
+  let x = 0;
+  let array = [];
+
+  let loop = function(field, args)
+  {
+    array.push(`${field} ${args}`);
+
+    x++;
+
+    Object.keys(table)[x] == undefined ? callback(array.join()) : loop(Object.keys(table)[x], table[Object.keys(table)[x]]);
+  };
+
+  Object.keys(table)[x] == undefined ? callback(true) : loop(Object.keys(table)[x], table[Object.keys(table)[x]]);
 }
 
 /****************************************************************************************************/
